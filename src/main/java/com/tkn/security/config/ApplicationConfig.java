8fa12037fdea4d91d1eabef3c80bf6a1d843c3b3
Davi <davi.frrs@gmail.com>
2023-05-29 10:02:21 -0300

    feat: Created jwtAuthenticationFilter

---
+++ src/main/java/com/tkn/security/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.tkn.security.config;
+
+import com.tkn.security.user.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final IUserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
